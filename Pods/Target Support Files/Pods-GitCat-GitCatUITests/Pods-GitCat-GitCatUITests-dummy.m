1d64574a7e51cfe9754dac758798d4ba52cdde96
Filip Culig <filip.culig@gmail.com>
2023-09-27 14:25:32 +0200

    Pod install

---
+++ Pods/Target Support Files/Pods-GitCat-GitCatUITests/Pods-GitCat-GitCatUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GitCat_GitCatUITests : NSObject
+@end
+@implementation PodsDummy_Pods_GitCat_GitCatUITests
+@end
---
+++ Pods/Target Support Files/Pods-GitCat/Pods-GitCat-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GitCat : NSObject
+@end
+@implementation PodsDummy_Pods_GitCat
+@end
